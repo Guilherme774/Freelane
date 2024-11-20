f96849124c6d293f1e0fbcdbbf3951566a0ff335
Guilherme774 <silva.guilhermee77@gmail.com>
2024-11-20 18:30:58 -0300

    <chore>[Freelane]: Start project modal

--- freelane/src/app/projects/projects-page.tsx
+++ freelane/src/app/projects/projects-page.tsx
@@ -1,5 +1,9 @@
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
 import { InfoCard } from "./components/info-card/info-card";
 import { ProjectCard } from "./components/project-card/project-card";
+import { Button } from "@/components/ui/button";
+import { Plus } from "lucide-react";
+import { Dialog, DialogContent, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
 
 export default function ProjectsPage() {
     return(
@@ -13,13 +17,44 @@ export default function ProjectsPage() {
 
             {/* Here should have project card list */}
 
-            <div className="grid grid-cols-3 gap-6">
-                {Array.from({ length: 5 }).map((_, i) => {
-                    return(
-                        <ProjectCard key={i} />
-                    )
-                })}
-            </div>
+            <Card>
+                <CardHeader className="flex flex-row justify-between">
+                    <span className="font-extrabold text-xl capitalize">Projects</span>
+                    
+                    <Dialog>
+                        <DialogTrigger asChild>
+                            <Button>
+                                <Plus className="w-4 h-4" />
+                                Add project
+                            </Button>
+                        </DialogTrigger>
+
+                        <DialogContent className="p-6 bg-white rounded shadow-lg">
+                            <DialogHeader>
+                                <DialogTitle className="text-lg text-bold mb-4">Add New Project</DialogTitle>
+                            </DialogHeader>
+                            <span>Hello World</span>
+
+                            <form action="">
+                                <DialogFooter>
+                                    <Button type="button" variant="outline">Cancel</Button>
+                                    <Button type="submit">Save</Button>
+                                </DialogFooter>
+                            </form>
+                        </DialogContent>
+                    </Dialog>
+                </CardHeader>
+
+                <CardContent>
+                    <div className="grid grid-cols-3 gap-6">
+                        {Array.from({ length: 5 }).map((_, i) => {
+                            return(
+                                <ProjectCard key={i} />
+                            )
+                        })}
+                    </div>
+                </CardContent>
+            </Card>
         </div>
     );
 }
